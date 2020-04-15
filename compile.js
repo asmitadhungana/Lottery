99847b26c7c3e71cdcf99dadf913fac9c4b9ab69
asmitadhungana <39458075+asmitadhungana@users.noreply.github.com>
2020-04-15 18:39:45 +0545

    set up the skeleton of our project

--- compile.js
+++ compile.js
@@ -2,8 +2,8 @@ const path = require("path");
 const fs = require("fs"); //fs = file system module in nodejs lib
 const solc = require("solc");
 
-const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
-const source = fs.readFileSync(inboxPath, "utf8"); //reading the content(raw source code) of the file Inbox.sol , utf8 is the encoding type of the file
+const lotteryPath = path.resolve(__dirname, "contracts", "Lottery.sol");
+const source = fs.readFileSync(lotteryPath, "utf8"); //reading the content(raw source code) of the file Inbox.sol , utf8 is the encoding type of the file
 
 //passing our source code to solc, 1 is the no of contracts we need to compile
-module.exports = solc.compile(source, 1).contracts[":Inbox"];
+module.exports = solc.compile(source, 1).contracts[":Lottery"];
--- deploy.js
+++ deploy.js
@@ -1,4 +1,4 @@
-const HDWalletProvider = require("truffle-hdwallet-provider");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require("web3");
 const { interface, bytecode } = require("./compile");
 
@@ -15,7 +15,7 @@ const deploy = async () => {
   console.log("Attempting to deploy from account", accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: "0x" + bytecode, arguments: ["Hi there!"] })
+    .deploy({ data: "0x" + bytecode })
     .send({ from: accounts[0] });
 
   console.log("Contract deployed to", result.options.address);
